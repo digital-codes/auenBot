576e80dbe70a978eb89a2bb557e44ba9573d63ef
Andreas Kugel <andreas.kugel@web.de>
2026-01-19 15:24:09 +0100

    botActions and tpa update

--- pyStates/botActions.py
+++ pyStates/botActions.py
@@ -95,10 +95,60 @@ class BotAction:
             return []
 
     def get_entity_features(self,name,key):
+        # check synonyms 
+        #'Erkennungsmerkmale', 'Habitat', 'Fortpflanzung', 'Größe', 'Links', 'Familie', 
+        # 'Gattung', 'Lebensraum', 'Klasse', 'Lebensweise', 'Nahrung', 'Feinde', 
+        # 'Lebenserwartung', 'Schutz', 'Wissenswertes', 'Blütezeit', 'Verwendung',
+        # 'Frucht', 'Vorkommen', 'Genießbarkeit', 'Ökologische Bedeutung', 'Giftigkeit', 
+        # 'Alter', 'Gewicht', 'Überwinterung', 'Verhalten', 'Paarung']
+        
+        # key from intent:
+            # tp_groesse, tp_habitat, tp_erkennungsmerkmale, tp_lateinischername, 
+            # tp_lebenserwartung, tp_fortpflanzung, tp_aussehen
+        searchImages = False
+
+        if "habitat" in key.lower():
+            searchKeys_ = ["Lebensraum", "Habitat"]
+        elif "lebensraum" in key.lower():
+            searchKeys_ = ["Lebensraum", "Habitat"]
+        elif "merkmale" in key.lower():
+            searchKeys_ = ["Erkennungsmerkmale", "Größe","Gewicht","Verhalten","Lebensweise"]
+            searchImages = True
+        elif "aussehen" in key.lower():
+            searchKeys_ = ["Größe"]
+            searchImages = True
+        elif "paarung" in key.lower():
+            searchKeys_ = ["Paarung", "Fortpflanzung"]
+        elif "fortpflanzung" in key.lower():
+            searchKeys_ = ["Paarung", "Fortpflanzung"]
+        elif ("größe" in key.lower()) or ("groesse" in key.lower()):
+            searchKeys_ = ["Größe"]
+        elif "tp_lateinischername" in key.lower():
+            searchKeys_ = ["Name_sci"]
+        else:
+            searchKeys_ = key.split("tp_")[-1]
+
+        # make sure we have leading capital letters
+        if isinstance(searchKeys_, str):
+            searchKeys = [searchKeys_[0].upper() + searchKeys_[1:].lower()]
+        else:
+            searchKeys = [a[0].upper() + a[1:].lower() for a in searchKeys_]
+            
         try:
             items = [e for e in self.data if name.lower() == (e.get('Name') or '').lower()]
             if items:
-                values = [f.get(key) for f in items if key in f]
+                values = []
+                for key in searchKeys:
+                    values.extend([f.get(key) for f in items if key in f and f.get(key)])
+                if searchImages:
+                    # also collect image links if available
+                    for f in items:
+                        if 'Links' in f and f['Links']:
+                            for l in f['Links']:
+                                img = l.get("img",None)
+                                if img:
+                                    values.append(img)
+                                    break
                 return values
             else:
                 print("No matching entity found for features:", name)
@@ -113,7 +163,7 @@ if __name__ == "__main__":
         result = action.extract_animal_or_plant(user_input)
         if result:
             print("1:", [r.get('Name') for r in result])
-            for f in ["Lebensraum","Merkmale","Links"]:
+            for f in ["Lebensraum","Merkmale","AUssehen"]:
                 name = result[0].get('Name')
                 features = action.get_entity_features(name,f)
                 if features:
